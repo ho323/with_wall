6d41ff0139d3e8576ddd487c9a194336102a4fac
Hoseong Kim <8536048@gmail.com>
2023-03-15 06:03:54 +0900

    피드 수정

--- lib/component/feed.dart
+++ lib/component/feed.dart
@@ -23,19 +23,84 @@ class Feed extends StatelessWidget {
         );
       },
       child: Container(
-        height: 300,
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: PRIMARY_COLOR,
-          ),
-          color: Colors.white,
-        ),
-        child: Center(
-          child: Text(
-            postNumber.toString(),
-          ),
+        padding: EdgeInsets.all(20),
+        height: 400,
+        // decoration: BoxDecoration(
+        //   border: Border.all(
+        //     color: PRIMARY_COLOR,
+        //   ),
+        //   color: Colors.white,
+        // ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    RenderCircle(),
+                    Text('닉네임'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.place, size: 15),
+                    Text('센터 위치'),
+                    SizedBox(width: 30),
+                  ],
+                ),
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              color: PRIMARY_COLOR,
+              child: Center(
+                child: Text('썸네일'),
+              ),
+            ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text('좋아요'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        RenderCircle(),
+                        RenderCircle(),
+                        RenderCircle(),
+                        SizedBox(width: 30),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget RenderCircle() {
+    return Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.red,
+            shape: BoxShape.circle,
+          ),
+          width: 20.0,
+          height: 20.0,
+        ),
+        SizedBox(width: 5),
+      ],
     );
   }
 }
